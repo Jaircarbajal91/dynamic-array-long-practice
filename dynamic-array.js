a695e0df498af1d54197fd4ae1c7460c73787334
Jair Carbajal <jair.carbajal91@gmail.com>
2022-05-10 13:24:37 -0700

    constructor and read implemented?

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,13 +1,17 @@
 class DynamicArray {
 
   constructor(defaultSize=4) {
-
+    this.capacity = defaultSize;
+    this.length = 0;
+    this.data = new Array(defaultSize);
     // Your code here
   }
 
   read(index) {
-
-    // Your code here
+    if (index <= this.length) {
+      return this.data[index];
+    }
+    return undefined;
   }
 
   push(val) {
